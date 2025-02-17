0ef84381c8a9777290ba544f733bd5b51653964d
evanbfeng <evanbfeng@tencent.com>
2025-02-17 13:04:27 +0800

    trail vertex and index

---
+++ Renderer/TrailRenderer.cpp
@@ -0,0 +1,8 @@
+//
+//  TrailRenderer.cpp
+//  ParticleSystem
+//
+//  Created by evanbfeng on 2025/2/17.
+//
+
+#include "TrailRenderer.hpp"
---
+++ Renderer/TrailRenderer.hpp
@@ -0,0 +1,13 @@
+//
+//  TrailRenderer.hpp
+//  ParticleSystem
+//
+//  Created by evanbfeng on 2025/2/17.
+//
+
+#ifndef TrailRenderer_hpp
+#define TrailRenderer_hpp
+
+#include <stdio.h>
+
+#endif /* TrailRenderer_hpp */
